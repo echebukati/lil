ecc425e3ce321d890f4568451cda37b173eb582e
Emmanuel Chebukati <echebukati@gmail.com>
2021-09-22 21:22:56 +0200

    Upload file to s3 with ID of insert

--- insert.php
+++ insert.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+require 'vendor/autoload.php';
 //require __DIR__ . '/vendor/autoload.php';
 session_start();
 if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
@@ -8,7 +13,7 @@ $username = ($_SESSION['username']);
 
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "fintech", "wC!viIkBek@6", "fintechdb");
+$link = mysqli_connect("fintechclouddb.cnydfygbrhuz.us-east-1.rds.amazonaws.com", "fintech", "wC!viIkBek@6", "fintechdb");
  
 // Check connection
 if ($link === false) {
@@ -28,10 +33,32 @@ if (isset($period)) {
  
 // attempt insert query execution
 $sql = "INSERT INTO transactions (user_id, amount, period, installments) VALUES ('$username', '100', '$period', '$installments')";
-if(mysqli_query($link, $sql)){
+if(mysqli_query($link, $sql)) {
+    $insertid = mysqli_insert_id($link);
     echo "Records added successfully.<br>";
     echo "You have received your <b>$100</b> loan!<br>";
     echo "You will pay back <b>$". $installments . "</b> for <b>".$period."</b> months.";
+
+    //STARTFILEUPLOAD
+    if(isset($_FILES['image'])){
+		$file_name = $_FILES['image']['name'];   
+		$temp_file_location = $_FILES['image']['tmp_name']; 
+
+		$s3 = new Aws\S3\S3Client([
+			'region'  => 'us-east-1',
+			'version' => 'latest'
+		]);		
+
+		$result = $s3->putObject([
+			'Bucket' => 'testfintechbucket',
+			'Key'    => $insertid.".pdf",
+			'SourceFile' => $temp_file_location			
+		]);
+
+		var_dump($result);
+	}
+    //ENDFILEUPLOAD
+
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
